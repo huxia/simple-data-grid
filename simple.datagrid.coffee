10da6b89929c42ba5a007fadf9aa58e6bd0cd4e0
Marco Braak <mbraak@ridethepony.nl>
2012-03-16 07:43:15 +0100

    Initial commit

---
+++ simple.datagrid.coffee
@@ -0,0 +1,355 @@
+$ = @jQuery
+
+
+slugify = (string) ->
+    return string.replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase()
+
+buildUrl = (url, query_parameters) ->
+    result = url
+
+    is_first = true
+    for key, value of query_parameters
+        if is_first
+            result += '?'
+            is_first = true
+        else
+            result += '&'
+
+        result += "#{ key }=#{ value }"
+
+    return result
+
+
+class ColumnInfo
+    constructor: (title, key, on_generate) ->
+        # The key parameter is optional. If it's empty, then it's defined as the slugified title.
+        @title = title
+        @key = key or slugify(title)
+        @on_generate = on_generate
+
+
+class Paginator
+    constructor: (element, number_of_columns, url, on_load_data) ->
+        @element = element
+        @number_of_columns = number_of_columns
+        @url = url
+        @on_load_data = on_load_data
+
+        @page = 1
+        @total_pages = 1
+
+        @_bindEvents()
+
+    _bindEvents: ->
+        @element.delegate('.paginator .first', 'click', $.proxy(this._handleClickFirstPage, this))
+        @element.delegate('.paginator .previous', 'click', $.proxy(this._handleClickPreviousPage, this))
+        @element.delegate('.paginator .next', 'click', $.proxy(this._handleClickNextPage, this))
+        @element.delegate('.paginator .last', 'click', $.proxy(this._handleClickLastPage, this))
+
+    removeEvents: ->
+        @element.undelegate('.paginator .first', 'click')
+        @element.undelegate('.paginator .previous', 'click')
+        @element.undelegate('.paginator .next', 'click')
+        @element.undelegate('.paginator .last', 'click')
+
+    buildHtml: ->
+        if not @total_pages or @total_pages == 1
+            return ''
+        else
+            html = "<tr><td class=\"paginator\" colspan=\"#{ @number_of_columns }\">"
+
+            if not @page or @page == 1
+                html += '<span class="first">first</span>'
+                html += '<span class="previous">previous</span>'
+            else
+                html += "<a href=\"#{ @getUrl(1) }\" class=\"first\">first</a>"
+                html += "<a href=\"#{ @getUrl(@page - 1) }\" class=\"previous\">previous</a>"
+
+            html += "page #{ @page } of #{ @total_pages }"
+
+            if not @page or @page == @total_pages
+                html += '<span class="next">next</span>'
+                html += '<span class="last"></span>'
+            else
+                html += "<a href=\"#{ @getUrl(@page + 1) }\" class=\"next\">next</a>"
+                html += "<a href=\"#{ @getUrl(@total_pages) }\" class=\"last\">last</a>"
+
+            html += "</td></tr>"
+            return html
+
+    _handleClickFirstPage: (e) ->
+        @_gotoPage(1)
+        return false
+
+    _handleClickPreviousPage: (e) ->
+        @_gotoPage(@page - 1)
+        return false
+
+    _handleClickNextPage: (e) ->
+        @_gotoPage(@page + 1)
+        return false
+
+    _handleClickLastPage: (e) ->
+        @_gotoPage(@total_pages)
+        return false
+
+    _gotoPage: (page) ->
+        if page <= @total_pages
+            @page = page
+            @on_load_data()
+
+    getUrl: (page) ->
+        if not page?
+            page = @page
+
+        if not page or page == 1
+            return @url
+        else
+            return @url + "?page=#{ page }"
+
+    getQueryParameters: ->
+        if not page?
+            page = @page
+
+        if not page or page == 1
+            return {}
+        else
+            return {page: page}
+
+
+$.widget("ui.simple_datagrid", {
+    options:
+        onGetData: null
+
+    _create: ->
+        @_generateColumnData()
+        @_createPaginator()
+        @_createDomElements()
+        @_bindEvents()
+        @_loadData()
+
+        @$selected_row = null
+        @parameters = {}
+
+    destroy: ->
+        @_removeDomElements()
+        @_removeEvents()
+
+        $.Widget.prototype.destroy.call(this)
+
+    getSelectedRow: ->
+        if @$selected_row
+            return @$selected_row.data('row')
+        else
+            return null
+
+    reload: ->
+        @_loadData()
+
+    setParameter: (key, value) ->
+        @parameters[key] = value
+
+    _generateColumnData: ->
+        generateFromThElements = =>
+            $th_elements = @element.find('th')
+            @columns = []
+            $th_elements.each(
+                (i, th) =>
+                    $th = $(th)
+
+                    title = $th.text()
+                    key = $th.data('key')
+
+                    @columns.push(
+                        new ColumnInfo(title, key)
+                    )
+            )
+
+        generateFromOptions = =>
+            @columns = []
+            for column in @options.columns
+                if typeof column == 'object'
+                    column_info = new ColumnInfo(column.title, column.key, column.on_generate)
+                else
+                    column_info = new ColumnInfo(column)
+
+                @columns.push(column_info)
+
+        if @options.columns
+            generateFromOptions()
+        else
+            generateFromThElements()
+
+    _createPaginator: ->
+        @paginator = new Paginator(
+            @element,
+            @columns.length,
+            @_getBaseUrl(),
+            $.proxy(@_loadData, this)
+        )
+
+    _createDomElements: ->
+        initTable = =>
+            @element.addClass('simple-data-grid')
+
+        initBody = =>
+            @$tbody = @element.find('tbody')
+
+            if @$tbody.length
+                @$tbody.empty()
+            else
+                @$tbody = $('<tbody></tbody>')
+                @element.append(@$tbody)
+
+        initFoot = =>
+            @$tfoot = @element.find('tfoot')
+
+            if @$tfoot.length
+                @$tfoot.empty()
+            else
+                @$tfoot = $('<tfoot></tfoot>')
+                @element.append(@$tfoot)
+
+        initHead = =>
+            @$thead = @element.find('thead')
+
+            if @$thead.length
+                @$thead.empty()
+            else
+                @$thead = $('<thead></thead>')
+                @element.append(@$thead)
+
+            html = '<tr>'
+            for column in @columns
+                html += "<th data-key=\"#{ column.key }\">#{ column.title }</th>"
+
+            html += '</tr>'
+            @$thead.append($(html))
+
+        initTable()
+        initHead()
+        initBody()
+        initFoot()
+
+    _removeDomElements: ->
+        @element.removeClass('simple-data-grid')
+        @$tbody.remove()
+        @$tbody = null
+
+    _bindEvents: ->
+        @element.delegate('tbody tr', 'click', $.proxy(this._clickRow, this))
+
+    _removeEvents: ->
+        @element.undelegate('tbody tr', 'click')
+        @paginator.removeEvents()
+
+    _getBaseUrl: ->
+        url = @options.url
+        if url
+            return url
+        else
+            return @element.data('url')
+
+    _clickRow: (e) ->
+        if @$selected_row
+            @$selected_row.removeClass('selected')
+
+        $tr = $(e.target).closest('tr')
+        $tr.addClass('selected')
+        @$selected_row = $tr
+
+        event = $.Event('datagrid.select')
+        @element.trigger(event)
+
+    _loadData: ->
+        query_parameters = $.extend(@parameters, @paginator.getQueryParameters())
+
+        if @options.onGetData
+            @options.onGetData(
+                query_parameters,
+                $.proxy(@_fillGrid, this)
+            )
+        else
+            url = buildUrl(
+                @paginator.url,
+                query_parameters
+            )
+
+            $.ajax({
+                url: url,
+                success: (result) =>
+                    @_fillGrid(result)
+                , datatType: 'json',
+                cache: false
+            })
+
+    _fillGrid: (data) ->
+        addRowFromObject = (row) =>
+            html = ''
+            for column in @columns
+                if column.key of row
+                    value = row[column.key]
+
+                    if column.on_generate
+                        value = column.on_generate(value, row)
+                else
+                    value = ''
+
+                html += "<td>#{ value }</td>"
+
+            return html
+
+        addRowFromArray = (row) =>
+            html = ''
+
+            for value, i in row
+                column = @columns[i]
+
+                if column.on_generate
+                    value = column.on_generate(value, row)
+
+                html += "<td>#{ value }</td>"
+
+            return html
+
+        generateTr = (row) =>
+            if row.id
+                data_string = " data-id=\"#{ row.id }\""
+            else
+                data_string = ""
+
+            return "<tr#{ data_string }>"
+
+        fillRows = (rows) =>
+            @$tbody.empty()
+
+            for row in rows
+                html = generateTr(row)
+
+                if $.isArray(row)
+                    html += addRowFromArray(row)
+                else
+                    html += addRowFromObject(row)
+
+                html += '</tr>'
+                $tr = $(html)
+                $tr.data('row', row)
+                @$tbody.append($tr)
+
+        fillFooter = =>
+            @$tfoot.html(
+                @paginator.buildHtml()
+            )
+
+        if $.isArray(data)
+            rows = data
+            @paginator.total_pages = 0
+        else if data.rows
+            rows = data.rows
+            @paginator.total_pages = data.total_pages or 0
+        else
+            rows = []
+
+        fillRows(rows)
+        fillFooter()
+})
---
+++ simple.datagrid.js
@@ -0,0 +1,384 @@
+(function() {
+  var $, ColumnInfo, Paginator, buildUrl, slugify;
+
+  $ = this.jQuery;
+
+  slugify = function(string) {
+    return string.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9\-]/g, '').toLowerCase();
+  };
+
+  buildUrl = function(url, query_parameters) {
+    var is_first, key, result, value;
+    result = url;
+    is_first = true;
+    for (key in query_parameters) {
+      value = query_parameters[key];
+      if (is_first) {
+        result += '?';
+        is_first = true;
+      } else {
+        result += '&';
+      }
+      result += "" + key + "=" + value;
+    }
+    return result;
+  };
+
+  ColumnInfo = (function() {
+
+    function ColumnInfo(title, key, on_generate) {
+      this.title = title;
+      this.key = key || slugify(title);
+      this.on_generate = on_generate;
+    }
+
+    return ColumnInfo;
+
+  })();
+
+  Paginator = (function() {
+
+    function Paginator(element, number_of_columns, url, on_load_data) {
+      this.element = element;
+      this.number_of_columns = number_of_columns;
+      this.url = url;
+      this.on_load_data = on_load_data;
+      this.page = 1;
+      this.total_pages = 1;
+      this._bindEvents();
+    }
+
+    Paginator.prototype._bindEvents = function() {
+      this.element.delegate('.paginator .first', 'click', $.proxy(this._handleClickFirstPage, this));
+      this.element.delegate('.paginator .previous', 'click', $.proxy(this._handleClickPreviousPage, this));
+      this.element.delegate('.paginator .next', 'click', $.proxy(this._handleClickNextPage, this));
+      return this.element.delegate('.paginator .last', 'click', $.proxy(this._handleClickLastPage, this));
+    };
+
+    Paginator.prototype.removeEvents = function() {
+      this.element.undelegate('.paginator .first', 'click');
+      this.element.undelegate('.paginator .previous', 'click');
+      this.element.undelegate('.paginator .next', 'click');
+      return this.element.undelegate('.paginator .last', 'click');
+    };
+
+    Paginator.prototype.buildHtml = function() {
+      var html;
+      if (!this.total_pages || this.total_pages === 1) {
+        return '';
+      } else {
+        html = "<tr><td class=\"paginator\" colspan=\"" + this.number_of_columns + "\">";
+        if (!this.page || this.page === 1) {
+          html += '<span class="first">first</span>';
+          html += '<span class="previous">previous</span>';
+        } else {
+          html += "<a href=\"" + (this.getUrl(1)) + "\" class=\"first\">first</a>";
+          html += "<a href=\"" + (this.getUrl(this.page - 1)) + "\" class=\"previous\">previous</a>";
+        }
+        html += "page " + this.page + " of " + this.total_pages;
+        if (!this.page || this.page === this.total_pages) {
+          html += '<span class="next">next</span>';
+          html += '<span class="last"></span>';
+        } else {
+          html += "<a href=\"" + (this.getUrl(this.page + 1)) + "\" class=\"next\">next</a>";
+          html += "<a href=\"" + (this.getUrl(this.total_pages)) + "\" class=\"last\">last</a>";
+        }
+        html += "</td></tr>";
+        return html;
+      }
+    };
+
+    Paginator.prototype._handleClickFirstPage = function(e) {
+      this._gotoPage(1);
+      return false;
+    };
+
+    Paginator.prototype._handleClickPreviousPage = function(e) {
+      this._gotoPage(this.page - 1);
+      return false;
+    };
+
+    Paginator.prototype._handleClickNextPage = function(e) {
+      this._gotoPage(this.page + 1);
+      return false;
+    };
+
+    Paginator.prototype._handleClickLastPage = function(e) {
+      this._gotoPage(this.total_pages);
+      return false;
+    };
+
+    Paginator.prototype._gotoPage = function(page) {
+      if (page <= this.total_pages) {
+        this.page = page;
+        return this.on_load_data();
+      }
+    };
+
+    Paginator.prototype.getUrl = function(page) {
+      if (!(page != null)) page = this.page;
+      if (!page || page === 1) {
+        return this.url;
+      } else {
+        return this.url + ("?page=" + page);
+      }
+    };
+
+    Paginator.prototype.getQueryParameters = function() {
+      var page;
+      if (!(typeof page !== "undefined" && page !== null)) page = this.page;
+      if (!page || page === 1) {
+        return {};
+      } else {
+        return {
+          page: page
+        };
+      }
+    };
+
+    return Paginator;
+
+  })();
+
+  $.widget("ui.simple_datagrid", {
+    options: {
+      onGetData: null
+    },
+    _create: function() {
+      this._generateColumnData();
+      this._createPaginator();
+      this._createDomElements();
+      this._bindEvents();
+      this._loadData();
+      this.$selected_row = null;
+      return this.parameters = {};
+    },
+    destroy: function() {
+      this._removeDomElements();
+      this._removeEvents();
+      return $.Widget.prototype.destroy.call(this);
+    },
+    getSelectedRow: function() {
+      if (this.$selected_row) {
+        return this.$selected_row.data('row');
+      } else {
+        return null;
+      }
+    },
+    reload: function() {
+      return this._loadData();
+    },
+    setParameter: function(key, value) {
+      return this.parameters[key] = value;
+    },
+    _generateColumnData: function() {
+      var generateFromOptions, generateFromThElements,
+        _this = this;
+      generateFromThElements = function() {
+        var $th_elements;
+        $th_elements = _this.element.find('th');
+        _this.columns = [];
+        return $th_elements.each(function(i, th) {
+          var $th, key, title;
+          $th = $(th);
+          title = $th.text();
+          key = $th.data('key');
+          return _this.columns.push(new ColumnInfo(title, key));
+        });
+      };
+      generateFromOptions = function() {
+        var column, column_info, _i, _len, _ref, _results;
+        _this.columns = [];
+        _ref = _this.options.columns;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          column = _ref[_i];
+          if (typeof column === 'object') {
+            column_info = new ColumnInfo(column.title, column.key, column.on_generate);
+          } else {
+            column_info = new ColumnInfo(column);
+          }
+          _results.push(_this.columns.push(column_info));
+        }
+        return _results;
+      };
+      if (this.options.columns) {
+        return generateFromOptions();
+      } else {
+        return generateFromThElements();
+      }
+    },
+    _createPaginator: function() {
+      return this.paginator = new Paginator(this.element, this.columns.length, this._getBaseUrl(), $.proxy(this._loadData, this));
+    },
+    _createDomElements: function() {
+      var initBody, initFoot, initHead, initTable,
+        _this = this;
+      initTable = function() {
+        return _this.element.addClass('simple-data-grid');
+      };
+      initBody = function() {
+        _this.$tbody = _this.element.find('tbody');
+        if (_this.$tbody.length) {
+          return _this.$tbody.empty();
+        } else {
+          _this.$tbody = $('<tbody></tbody>');
+          return _this.element.append(_this.$tbody);
+        }
+      };
+      initFoot = function() {
+        _this.$tfoot = _this.element.find('tfoot');
+        if (_this.$tfoot.length) {
+          return _this.$tfoot.empty();
+        } else {
+          _this.$tfoot = $('<tfoot></tfoot>');
+          return _this.element.append(_this.$tfoot);
+        }
+      };
+      initHead = function() {
+        var column, html, _i, _len, _ref;
+        _this.$thead = _this.element.find('thead');
+        if (_this.$thead.length) {
+          _this.$thead.empty();
+        } else {
+          _this.$thead = $('<thead></thead>');
+          _this.element.append(_this.$thead);
+        }
+        html = '<tr>';
+        _ref = _this.columns;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          column = _ref[_i];
+          html += "<th data-key=\"" + column.key + "\">" + column.title + "</th>";
+        }
+        html += '</tr>';
+        return _this.$thead.append($(html));
+      };
+      initTable();
+      initHead();
+      initBody();
+      return initFoot();
+    },
+    _removeDomElements: function() {
+      this.element.removeClass('simple-data-grid');
+      this.$tbody.remove();
+      return this.$tbody = null;
+    },
+    _bindEvents: function() {
+      return this.element.delegate('tbody tr', 'click', $.proxy(this._clickRow, this));
+    },
+    _removeEvents: function() {
+      this.element.undelegate('tbody tr', 'click');
+      return this.paginator.removeEvents();
+    },
+    _getBaseUrl: function() {
+      var url;
+      url = this.options.url;
+      if (url) {
+        return url;
+      } else {
+        return this.element.data('url');
+      }
+    },
+    _clickRow: function(e) {
+      var $tr, event;
+      if (this.$selected_row) this.$selected_row.removeClass('selected');
+      $tr = $(e.target).closest('tr');
+      $tr.addClass('selected');
+      this.$selected_row = $tr;
+      event = $.Event('datagrid.select');
+      return this.element.trigger(event);
+    },
+    _loadData: function() {
+      var query_parameters, url,
+        _this = this;
+      query_parameters = $.extend(this.parameters, this.paginator.getQueryParameters());
+      if (this.options.onGetData) {
+        return this.options.onGetData(query_parameters, $.proxy(this._fillGrid, this));
+      } else {
+        url = buildUrl(this.paginator.url, query_parameters);
+        return $.ajax({
+          url: url,
+          success: function(result) {
+            return _this._fillGrid(result);
+          },
+          datatType: 'json',
+          cache: false
+        });
+      }
+    },
+    _fillGrid: function(data) {
+      var addRowFromArray, addRowFromObject, fillFooter, fillRows, generateTr, rows,
+        _this = this;
+      addRowFromObject = function(row) {
+        var column, html, value, _i, _len, _ref;
+        html = '';
+        _ref = _this.columns;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          column = _ref[_i];
+          if (column.key in row) {
+            value = row[column.key];
+            if (column.on_generate) value = column.on_generate(value, row);
+          } else {
+            value = '';
+          }
+          html += "<td>" + value + "</td>";
+        }
+        return html;
+      };
+      addRowFromArray = function(row) {
+        var column, html, i, value, _len;
+        html = '';
+        for (i = 0, _len = row.length; i < _len; i++) {
+          value = row[i];
+          column = _this.columns[i];
+          if (column.on_generate) value = column.on_generate(value, row);
+          html += "<td>" + value + "</td>";
+        }
+        return html;
+      };
+      generateTr = function(row) {
+        var data_string;
+        if (row.id) {
+          data_string = " data-id=\"" + row.id + "\"";
+        } else {
+          data_string = "";
+        }
+        return "<tr" + data_string + ">";
+      };
+      fillRows = function(rows) {
+        var $tr, html, row, _i, _len, _results;
+        _this.$tbody.empty();
+        _results = [];
+        for (_i = 0, _len = rows.length; _i < _len; _i++) {
+          row = rows[_i];
+          html = generateTr(row);
+          if ($.isArray(row)) {
+            html += addRowFromArray(row);
+          } else {
+            html += addRowFromObject(row);
+          }
+          html += '</tr>';
+          $tr = $(html);
+          $tr.data('row', row);
+          _results.push(_this.$tbody.append($tr));
+        }
+        return _results;
+      };
+      fillFooter = function() {
+        return _this.$tfoot.html(_this.paginator.buildHtml());
+      };
+      if ($.isArray(data)) {
+        rows = data;
+        this.paginator.total_pages = 0;
+      } else if (data.rows) {
+        rows = data.rows;
+        this.paginator.total_pages = data.total_pages || 0;
+      } else {
+        rows = [];
+      }
+      fillRows(rows);
+      return fillFooter();
+    }
+  });
+
+}).call(this);
