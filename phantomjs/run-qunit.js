2e594d887f3492af103a9891d6d859e536d985ed
Marco Braak <mbraak@ridethepony.nl>
2012-08-01 12:38:13 +0200

    Changed timeout in phantomjs test to 10s

--- phantomjs/run-qunit.js
+++ phantomjs/run-qunit.js
@@ -71,6 +71,8 @@ page.open(system.args[1], function(status){
                 return 10000;
             });
             phantom.exit((parseInt(failedNum, 10) > 0) ? 1 : 0);
-        });
+        },
+        10000
+        );
     }
 });
